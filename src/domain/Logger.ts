bedaf149048cfdacbf3e5d134d2291fd20b1bb14
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-08-14 11:21:25 -0300

    Added color to the logger

--- src/domain/Logger.ts
+++ src/domain/Logger.ts
@@ -10,14 +10,14 @@ export class Logger {
 
     warn(message: string) {
         const date = new Date()
-        const text = `WARNING: ${this.getDateString(date)} - ${message}`
+        const text = `\x1b[33mWARNING: ${this.getDateString(date)} - ${message}\x1b[0m`
 
         console.warn(text)
     }
 
     error(message: string) {
         const date = new Date()
-        const text = `ERROR: ${this.getDateString(date)} - ${message}`
+        const text = `\x1b[31mERROR: ${this.getDateString(date)} - ${message}\x1b[0m`
 
         console.error(text)
     }
